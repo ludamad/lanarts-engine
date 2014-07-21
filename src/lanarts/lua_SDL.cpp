a38f5d71a3a59b910d76a00444791dea92d452de
Adam <domuradical@gmail.com>
2014-07-21 18:48:45 +0200

    Link SDL and expose copy paste functionality

--- src/lanarts/lua_SDL.cpp
+++ src/lanarts/lua_SDL.cpp
@@ -1,5 +1,7 @@
 #include <cassert>
+#include <cstdlib>
 #include <SDL.h>
+#include <string>
 
 #include <luawrap/luawrap.h>
 
@@ -7,9 +9,27 @@ static int GetModState() {
     return (int)SDL_GetModState();
 }
 
+static bool IsLCTRLDown() {
+    return SDLK_LCTRL & (int)SDL_GetModState();
+}
+
+static bool IsRCTRLDown() {
+    return SDLK_RCTRL & (int)SDL_GetModState();
+}
+
+static std::string GetClipboardText() {
+    const char* text = SDL_GetClipboardText();
+    std::string copy = text;
+    free((void*)text);
+    return copy;
+}
+
 int luaopen_SDL(lua_State *L) {
     LuaValue module = LuaValue::newtable(L);
     module["GetModState"].bind_function(GetModState);
+    module["IsLCTRLDown"].bind_function(IsLCTRLDown);
+    module["IsRCTRLDown"].bind_function(IsRCTRLDown);
+    module["GetClipboardText"].bind_function(GetClipboardText);
     module.push();
     return 1;
 }
